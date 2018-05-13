198a4620e5c5d90d4a30c74b2ed662caecc334f1
Raymond <rs01247@gmail.com>
2018-05-12 19:20:24 -0700

    objects included to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,86 @@
 window.onload = function () {
 
-
     // DECLARING VARIABLES
     var yourChar;
-    var otherChar;
+    var oppChar;
     var attackBtn;
     var messageBox;
-    var startClick;
 
+
+    function init() {
+
+    }
+
+    var characters = [cloud, barret, aeris, tifa, vincent];
+
+    var cloud = {
+        name: "cloud",
+        health: 130,
+        attack: 15,
+        counter: 10,
+    }
+
+    var barret = {
+        name: "barret",
+        health: 150,
+        attack: 20,
+        counter: 15,
+    }
+
+    var aeris = {
+        name: "aeris",
+        health: 100,
+        attack: 8,
+        counter: 10,
+    }
+
+    var tifa = {
+        name: "tifa",
+        health: 120,
+        attack: 10,
+        counter: 10,
+    }
+
+    var vincent = {
+        name: "vincent",
+        health: 130,
+        attack: 10,
+        counter: 20,
+    }
+
+    //FUNCTION THAT BEGINS GAME ON CHARACTER SELECTION
     $(".char-box").on("click", function () {
-        if (!startClick) {
 
-            startClick = this;
+        //yourChar BEGINS FALSE, BUTTON CLICK SETS yourChar TO TRUE
+        if (!yourChar) {
+            yourChar = this;
             $("#my-char").html("<h4>Your Character</h4>");
-            $(this).data("startClick", true);
+            $(this).data("yourChar", true);
             $(this).attr("id", "player-char");
-
             console.log(this);
 
+            //SEND REMAINING CHARACTERS TO ENEMY AVAILABLE SECTION
             $.each($(".char-box"), function (key, element) {
-                if (startClick !== element) {
+                if (yourChar !== element) {
                     $("#enemies-avail").append(element);
-                    $(element).attr("id", "select-char");  
+                    $(element).attr("id", "select-char");
                 }
-                console.log(element);
             });
         }
 
+        //SELECTED ENEMY GOES TO DEFENDER SECTION
         else {
             $("#defender").append(this);
             $(this).attr("id", "opp-char");
+            oppChar = this;
+            console.log(this);
         }
     });
 
-
-
-
-
-
-
-    // $(".char-box").on("click", function () {
-    //     $("#.char-box").data("clicked", true);
-    // });
-
-    // if ($(".char-box").data("clicked")) {
-    //     $(".char-box").appendTo("#enemy-avail");
-    // }
-
-
-
-
-
-
-
-
-
-
+    $("#attackBtn").on("click", function () {
+        console.log(yourChar);
+        console.log(oppChar);
+    });
 
 
 
