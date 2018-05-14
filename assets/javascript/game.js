d153533e399808c9e4158cd958b949b0e40c5860
Raymond <rs01247@gmail.com>
2018-05-13 17:20:21 -0700

    object connected to button push, cleaning up win condition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,23 @@
 window.onload = function () {
 
     //INITIAL STATE
-    $("#restart-btn").hide();
-
+    // $("#restart-btn").hide();
 
     // DECLARING VARIABLES
+    var startBox;
     var yourChar;
     var oppChar;
-    var messageBox;
+    var activeBattle = false;
     var enemies = [];
     var playerwin;
-
-    function init() {
-
-    }
-
-    function updatePlayer () {
-        //FIX THIS LINK, NEEDS TO GO TO PLAYERCHOSEN (CHARACTER.HEALTH)
-        $("#char-health").text(character.health);
-    }
-
-    function updateOpp () {
-
-    }
-
-    function youWin () {
-        
-    }
-
-    var characters = [cloud, barret, aeris, tifa, vincent];
+    var battleTheme = new Audio("./assets/audio/battletheme.mp3");
+    var fanfare = new Audio("./assets/audio/fanfare.mp3");
 
     var cloud = {
         name: "Cloud",
         health: 130,
         attack: 15,
+        baseAttack: 15,
         counter: 10,
     }
 
@@ -41,6 +25,7 @@ window.onload = function () {
         name: "Barret",
         health: 180,
         attack: 20,
+        baseAttack: 20,
         counter: 15,
     }
 
@@ -48,6 +33,7 @@ window.onload = function () {
         name: "Aeris",
         health: 100,
         attack: 8,
+        baseAttack: 8,
         counter: 10,
     }
 
@@ -55,6 +41,7 @@ window.onload = function () {
         name: "Tifa",
         health: 120,
         attack: 10,
+        baseAttack: 10,
         counter: 10,
     }
 
@@ -62,24 +49,33 @@ window.onload = function () {
         name: "Vincent",
         health: 140,
         attack: 10,
+        baseAttack: 10,
         counter: 20,
     }
 
+    var characters = [cloud, barret, aeris, tifa, vincent];
+
     //FUNCTION THAT BEGINS GAME ON CHARACTER SELECTION
     $(".char-box").on("click", function () {
+        battleTheme.play();
 
         //yourChar BEGINS FALSE, BUTTON CLICK SETS yourChar TO TRUE
-        if (!yourChar) {
-            yourChar = this;
+        if (!activeBattle) {
             $("#my-char").html("<h4>Your Character</h4>");
-            $(this).data("yourChar", true);
+
+            startBox = this;
+            yourChar = eval($(this).attr("character"));
+            // $(this).data("yourChar", true);
             $(this).attr("id", "player-char");
-            console.log(this);
+            activeBattle = true;
+            console.log(startBox);
+            console.log(yourChar);
 
             //SEND REMAINING CHARACTERS TO ENEMY AVAILABLE SECTION
             $.each($(".char-box"), function (key, element) {
-                if (yourChar !== element) {
+                if (startBox !== element) {
                     $("#enemies-avail").append(element);
+                    $(element).attr("character");
                     $(element).attr("id", "select-char");
                 }
             });
@@ -89,19 +85,43 @@ window.onload = function () {
         else {
             $("#defender").append(this);
             $(this).attr("id", "opp-char");
-            oppChar = this;
-            console.log(this);
+            oppChar = eval($(this).attr("character"));
+            console.log(oppChar);
         }
     });
 
     $("#attackBtn").on("click", function () {
-        console.log(yourChar);
-        console.log(oppChar);
+        if (activeBattle == true) {
+            oppChar.health -= yourChar.attack;
+            yourChar.attack += yourChar.baseAttack;
+            updateOpp()
+            console.log(yourChar);
+            console.log(oppChar);
+        }
     });
 
-    $("#restart-btn").on("click", function() {
+
+
+    $("#restart-btn").on("click", function () {
         location.reload();
     });
 
+
+
+    function init() {
+
+    }
+
+    function updatePlayer() {
+        $("#char-health").text(yourChar.health);
+    }
+
+    function updateOpp() {
+        $("#char-health").text(oppChar.health);
+    }
+
+    function youWin() {
+
+    }
 
 }
